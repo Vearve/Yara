5df61a7356957b2f90855f6b1407dafbfec8ba14
MansLikeNight <evabloo2342@gmail.com>
2026-03-09 09:59:10 +0200

    Add HCM merge migration to resolve conflicting leaf nodes

---
+++ apps/hcm/migrations/0007_merge_0004_seed_lookup_values_0006_department_workspace_alter_employee_department.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hcm', '0004_seed_lookup_values'),
+        ('hcm', '0006_department_workspace_alter_employee_department'),
+    ]
+
+    operations = [
+    ]
